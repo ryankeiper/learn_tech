c79d8daa64fedc645a43d812b88231e74f9fefc0
Freetown5 <iconteh21@yahoo.com>
2015-04-21 14:13:57 -0400

    devise and results routes

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,2 +1,24 @@
 class ResultsController < ApplicationController
+  def index
+  end
+
+  def show
+    @result = Result.find(params[:id])
+  end
+
+  def new
+    @result = Result.new
+  end
+
+  def result_params
+    params.require(:result).permit(:category, :description)
+  end
+
 end
+
+# create_table "results", force: :cascade do |t|
+#   t.string   "category"
+#   t.text     "description"
+#   t.datetime "created_at",  null: false
+#   t.datetime "updated_at",  null: false
+# end
