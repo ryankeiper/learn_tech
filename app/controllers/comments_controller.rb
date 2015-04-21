1c35b66fee606ad4eade37983b135c1446d7129f
Adrian Maseda <amaseda@gmail.com>
2015-04-21 14:42:42 -0400

    Created comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,41 @@
 class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new( comment_params )
+    if @comment.save
+      redirect_to result_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @comment = Comment.find( params[:id] )
+  end
+
+  def update
+    @comment = Comment.find( params[:id] )
+    if @comment.update ( comment_params )
+      redirect_to result_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    # @result = Result.find(params[:id])
+    @comment = Comment.find( params[:id] )
+    @comment.destroy
+    redirect_to result_path
+  end
+
+  private
+
+  def comment_params
+    params.require( :comment ).permit( :body )
+  end
 end
