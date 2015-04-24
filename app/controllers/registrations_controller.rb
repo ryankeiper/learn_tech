c4cebdca2fc002f9a2f0b423f3ad5383fed2e758
Adrian Maseda <amaseda@gmail.com>
2015-04-23 20:19:36 -0400

    Fixed css comment clash.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,4 +9,9 @@ class RegistrationsController < Devise::RegistrationsController
   # def account_update_params
   #   params.require(:user).permit(:user_name, :email, :password, :password_confirmation, :current_password)
   # end
+
+  def after_sign_up_path_for(resource)
+    '/results/2'
+  end
+
 end
